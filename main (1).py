7d7db6907ceee0325e578a66ef5f81efe0c8c127
Sujitha123456 <144473036+Sujitha123456@users.noreply.github.com>
2023-10-10 19:54:41 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,27 +1,5 @@
-class Student:
-
-  def __init__(self, name, roll_number, cgpa):
-    self.name = name
-    self.roll_number = roll_number
-    self.cgpa = cgpa
-
-
-def sort_students(student_list):
-  sorted_students = sorted(student_list,
-                           key=lambda student: student.cgpa,
-                           reverse=True)
-  return sorted_students
-
-students = [
-    Student("Abi", "A123", 7.8),
-    Student("Anu", "A124", 8.9),
-    Student("Sowmiya", "A125", 9.1),
-    Student("ravi", "A126", 9.9),
-]
-
-sorted_students = sort_students(students)
-
-for student in sorted_students:
-  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
-                                                     student.roll_number,
-                                                     student.cgpa))
+year = int(input("Enter a year:"))
+if (year % 4 == 0):
+  print("{} is a leap year ".format(year))
+else:
+  print("{} is not a leap year".format(year))
